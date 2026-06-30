9b74981ffc38df5717bc949ffd94b0083aa694d5
pthnTrader <pthntrader@yahoo.com>
2026-06-30 20:06:33 +0200

    Test läuft nach Implementierung grün

--- A2/shopping_cart.py
+++ A2/shopping_cart.py
@@ -1,3 +1,11 @@
 class ShoppingCart:
     def __init__(self):
-        pass
+        self.items = []
+        self.empty = True
+        self.total = 0.0
+
+    def get_total(self) -> float:
+        return self.total
+    
+    def is_empty(self) -> bool:
+        return self.empty
